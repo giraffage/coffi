f7bf621b49a0f02a63a84f87cc31838558274046
Gioia <gioia@Gioias-MacBook-Pro.local>
2019-03-19 23:41:50 +0000

    added navbar with bootstrap
    I want the search bar to be separate and not too sure about the navbar.. We could try to restyle it in css

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@ let mainNav = document.getElementById('js-menu');
 
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
-navBarToggle.addEventListener('click', function () {
+navBarToggle.addEventListener('click', function() {
     mainNav.classList.toggle('active');
 })
